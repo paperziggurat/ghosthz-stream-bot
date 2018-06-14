9eb3145bee5b1a7950772d31410e243d4abc2a99
wes <wescarterdev@fastmail.com>
2018-06-14 10:23:35 -0500

    style cleanup

--- src/Bot.java
+++ src/Bot.java
@@ -2,20 +2,16 @@ import net.moraleboost.streamscraper.ScrapeException;
 import org.jibble.pircbot.IrcException;
 import org.jibble.pircbot.PircBot;
 import org.jibble.pircbot.User;
-
 import java.util.ArrayList;
 import java.util.TimerTask;
-
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Bot extends PircBot {
-
     private String nickname, server, channel, streamAddress, sourceAddress;
     private String currentlyPlaying;
 
-    public Bot(String nick, String serv, String chan, String stream, String src){
+    public Bot(String nick, String serv, String chan, String stream, String src) {
         nickname = nick;
         server = serv;
         channel = chan;
@@ -30,129 +26,132 @@ public class Bot extends PircBot {
         this.joinChannel(channel);
     }
 
-    public void onMessage(String channel, String sender, String login, String hostname, String message){
-        if (message.contains("!help")){
+    public void onMessage(String channel, String sender, String login, String hostname, String message) {
+        if (message.contains("!help")) {
             displayCommands(sender, message);
         }
 
-        if (message.equalsIgnoreCase("!song")){
+        if (message.equalsIgnoreCase("!song")) {
             StreamParser stream = new StreamParser(streamAddress);
             msg("Currently Playing: " + stream.getSongTitle());
         }
 
-        if (message.equalsIgnoreCase("!listeners")){
+        if (message.equalsIgnoreCase("!listeners")) {
             StreamParser stream = new StreamParser(streamAddress);
             msg(stream.getListenerCount() + "");
         }
 
-        if (message.equalsIgnoreCase("!peaklisteners")){
+        if (message.equalsIgnoreCase("!peaklisteners")) {
             StreamParser stream = new StreamParser(streamAddress);
             msg(stream.getPeakListenerCount() + "");
         }
 
-        if (message.equalsIgnoreCase("!maxlisteners")){
+        if (message.equalsIgnoreCase("!maxlisteners")) {
             StreamParser stream = new StreamParser(streamAddress);
             msg(stream.getMaxListenerCount() + "");
         }
 
-        if (message.equalsIgnoreCase("!source")){
+        if (message.equalsIgnoreCase("!source")) {
             msg(sourceAddress);
         }
 
-        if (message.contains("!kick") && isAuthentic(sender)){
+        if (message.contains("!kick") && isAuthentic(sender)) {
             String[] command = message.split(" ");
             String userToKick = command[1];
-            if (isUser(userToKick)){
+            
+            if (isUser(userToKick)) {
                 kick(channel, userToKick);
                 msg("Kicking " + userToKick + " from channel.");
-            }
-            else {
+            } else {
                 msg(userToKick + " is not in the channel.");
             }
         }
 
-        if (message.contains("!deop") && isAuthentic(sender)){
+        if (message.contains("!deop") && isAuthentic(sender)) {
             String[] command = message.split(" ");
             String userToDeOp = command[1];
-            if (isUser(userToDeOp)){
+            
+            if (isUser(userToDeOp)) {
                 msg("Removing operator privileges from " + userToDeOp + ".");
                 deOp(channel, userToDeOp);
-            }
-            else {
+            } else {
                 msg(userToDeOp + " is not in the channel.");
             }
-
         }
 
-        if (message.contains("!op") && isAuthentic(sender)){
+        if (message.contains("!op") && isAuthentic(sender)) {
             String[] command = message.split(" ");
             String userToOp = command[1];
-            if (isUser(userToOp)){
+            
+            if (isUser(userToOp)) {
                 msg("Granting operator privileges to " + userToOp + ".");
                 op(channel, userToOp);
-            }
-            else {
+            } else {
                 msg(userToOp + " is not in the channel.");
             }
-
         }
-
     }
 
-    public void msg(String msg){
+    public void msg(String msg) {
         sendMessage(channel, msg);
     }
 
-    public void pmsg(String sender, String msg){
+    public void pmsg(String sender, String msg) {
         sendMessage(sender, msg);
     }
 
-    public void displayCommands(String sender, String helpRequest){
+    public void displayCommands(String sender, String helpRequest) {
         String[] message = helpRequest.split(" ");
-        if (message.length < 2){
+        
+        if (message.length < 2) {
             pmsg(sender, "For help with op commands, enter !help op");
             pmsg(sender, "For help with radio commands, enter !help radio");
             pmsg(sender, "For help with weather commands, enter !help weather");
         }
-        if (message[1].equals("op")){
+        
+        if (message[1].equals("op")) {
             pmsg(sender, "!kick [user]");
             pmsg(sender, "!op [user]");
             pmsg(sender, "!deop[user]");
         }
+        
         if (message[1].equals("radio")){}
         if (message[1].equals("weather")){}
     }
 
     public void updateCurrentSong() throws URISyntaxException, ScrapeException {
         StreamParser stream = new StreamParser(streamAddress);
+        
         if (!currentlyPlaying.equals(stream.getSongTitle())){
             currentlyPlaying = stream.getSongTitle();
             //sendMessage(channel, "Currently playing: " + currentlyPlaying);
             setTopic(channel, "Currently playing: " + currentlyPlaying);
         }
-
     }
 
-    private boolean isAuthentic(String nick){
+    private boolean isAuthentic(String nick) {
         User[] users = getUsers(channel);
-        for (User user: users){
-            if (user.getNick().equals(nick)){
-                if (user.isOp()){
+        
+        for (User user: users) {
+            if (user.getNick().equals(nick)) {
+                if (user.isOp()) {
                     return true;
                 }
             }
         }
+        
         return false;
     }
 
-    private boolean isUser(String nick){
+    private boolean isUser(String nick) {
         User[] users = getUsers(channel);
-        for (User user: users){
-            if (user.getNick().equals(nick)){
+        
+        for (User user: users) {
+            if (user.getNick().equals(nick)) {
                 return true;
             }
         }
+        
         return false;
     }
-
 }
